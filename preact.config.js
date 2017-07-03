1039d32a94a5ec57235c7df2e7a4fe0e7d9a79df
Andrew H. Carpenter <drewwcarpenter@gmail.com>
2017-07-03 04:05:00 -0400

    use image-webpack-loader, trim up font set pulled by GoogleFontsPlugin

--- preact.config.js
+++ preact.config.js
@@ -7,16 +7,54 @@
 //  * @param {WebpackConfigHelpers} helpers - object with useful helpers when working with config.
 //  *
 import GoogleFontsPlugin from 'google-fonts-webpack-plugin';
+import AutoDllPlugin from 'autodll-webpack-plugin';
 
 export default function (config, env, helpers) {
   config.plugins = config.plugins.concat([
+    // new AutoDllPlugin({
+    //   inject: true, // will inject the DLL bundles to index.html
+    //   context: __dirname,
+    //   filename: '[name]_[hash].js',
+    //   entry: {
+    //     vendor: "/node_modules"
+    //   }
+    // }),
     new GoogleFontsPlugin({
+      formats: ["woff2"],
       fonts: [
-        { family: "Open Sans", variants: [ "400", "300", "700", "800", "400italic"] }
+        { family: "Open Sans", variants: [ "300", "700"] }
       ]
-      /* ...options */
-    })]
-  )
+    })
+  ])
 
-  return config
+  config.module.loaders = config.module.loaders.concat([
+    {test: /\.(gif|png|jpe?g|jpg|svg)$/i, loader: 'file-loader'},
+    {
+      test: /\.(gif|png|jpe?g|jpg|svg)$/i,
+      loader: 'image-webpack-loader',
+      query: {
+        progressive: true,
+        mozjpeg: {
+          quality: 65
+        },
+        svgo:{
+          plugins: [
+            {
+              removeViewBox: false
+            },
+            {
+              removeEmptyAttrs: false
+            }
+          ]
+        },
+        gifsicle: {
+          interlaced: true,
+          optimizationLevel: 3
+        }
+      }
+    }
+  ])
 }
+
+
+
