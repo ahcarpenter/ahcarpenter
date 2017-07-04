b4af127cd53a6a1efbf60d3edb5776710e1df8ce
Andrew H. Carpenter <drewwcarpenter@gmail.com>
2017-07-04 17:16:21 -0400

    ensure path to App module correct

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ import "font-awesome/css/font-awesome.min"
 import "./style/style.default"
 import "./style"
 
-import App from './components/app'
+import App from './components/App'
 
 export default () => <App /> ;
