6a69d8c1f47da26c19688d93934e5fca7d30e8d6
Andrew H. Carpenter <drewwcarpenter@gmail.com>
2017-07-29 04:13:41 -0400

    use custom template

--- routes/Home/index.js
+++ routes/Home/index.js
@@ -32,15 +32,15 @@ export default class Home extends Component {
 
 	render(_, { auxillarySectionsHidden, aboutHidden }) {
     const auxillarySections = aboutHidden ? [
-        <Portfolio key={0} />,
-        <Waypoint onEnter={this.onAboutEnteredViewport}>
-          <div id="about-and-footer-sections-placeholder"></div>
-        </Waypoint>
-      ] : [
-        <Portfolio key={0} />,
-        <About key={1} />,
-        <Footer key={2} />
-      ]
+      <Portfolio key={0} />,
+      <Waypoint onEnter={this.onAboutEnteredViewport}>
+        <div id="about-and-footer-sections-placeholder"></div>
+      </Waypoint>
+    ] : [
+      <Portfolio key={0} />,
+      <About key={1} />,
+      <Footer key={2} />
+    ]
 
     return (
 			<div class={style.home} rel="prefetch">
